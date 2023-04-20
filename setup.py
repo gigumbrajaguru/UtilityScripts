9af7198315884f0e3b6c0e7bf164360e2c53bf09
Gigum Bandara Rajaguru <gigumbrajaguru@gmail.com>
2023-04-20 14:31:42 +0530

    Reformatted

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ def scheduled_job_power():
     print("Job is running", flush=True)
     retirve_data_power_cut.EventHandle().create_event()
 
+
 schedule.every().day.at("06:00").do(scheduled_job_power)
 
 while True:
